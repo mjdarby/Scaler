7cb206b454789c5233597c4e53c2bc3a4826339b
Matthew Darby <me@mjdarby.net>
2015-07-21 00:49:27 +0100

    Thinking about it, I'm basically almost done. Enough time for another Brooklyn Nine-Nine I think.

--- app/scale/scale.js
+++ app/scale/scale.js
@@ -4,11 +4,11 @@ var major_progression = [0, 2, 4, 5, 7, 9, 11, 12, 11, 9, 7, 5, 4, 2, 0];
 var minor_progression = [0, 2, 3, 5, 7, 8, 11, 12, 11, 8, 7, 5, 3, 2, 0];
 var melodic_minor_progression = [0, 2, 3, 5, 7, 9, 11, 12, 10, 8, 7, 5, 3, 2, 0];
 
-var major_fifths = ['C', 'G', 'D', 'A', 'E', 'B', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F'];
-var minor_fifths = ['A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'Bb', 'F', 'C', 'G', 'D'];
+var major_fifths = ['C', 'G', 'D', 'A', 'E', 'B', 'G♭', 'D♭', 'A♭', 'E♭', 'B♭', 'F'];
+var minor_fifths = ['A', 'E', 'B', 'F♯', 'C♯', 'G♯', 'D♯', 'B♭', 'F', 'C', 'G', 'D'];
 
-var major_chromatic = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
-var minor_chromatic = ['A', 'Bb', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+var major_chromatic = ['C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭', 'A', 'B♭', 'B'];
+var minor_chromatic = ['A', 'B♭', 'B', 'C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯'];
 
 var myFace = function() {
   return "what";
@@ -27,14 +27,39 @@ angular.module('myApp.scale', ['ngRoute', 'ngCookies'])
   var cookie = $cookies.getObject('scales') || {};
   $scope.major = "";
   $scope.tempo = 80;
+  $scope.tempoIncrease = 4;
   $scope.selected = cookie.hasOwnProperty('selected') ? cookie.selected : [];
+  $scope.scalesPerDay = cookie.hasOwnProperty('scalesPerDay') ? cookie.selected : 3;
   $scope.majors = major_chromatic;
   $scope.minors = minor_chromatic;
 
-  $scope.addToList = function(key) {
-    var newElement = {key: key, tempo: $scope.tempo}
+  $scope.addToList = function(key, majorminor) {
+    var newElement = {key: key + " " + majorminor, tempo: parseInt($scope.tempo, 10)};
     $scope.selected.push(newElement);
     cookie.selected = $scope.selected;
     $cookies.putObject('scales', cookie);
   };
+
+  $scope.removeFromList = function(item) {
+    var index = $scope.selected.indexOf(item);
+    if (index > -1)
+    {
+      $scope.selected.splice(index, 1);
+    }
+    cookie.selected = $scope.selected;
+    $cookies.putObject('scales', cookie);
+  };
+
+  $scope.increaseTempo = function(item) {
+    var index = $scope.selected.indexOf(item);
+    if (index > -1)
+    {
+      $scope.selected.splice(index, 1);
+      item.tempo += $scope.tempoIncrease;
+      $scope.selected.push(item);
+    }
+    cookie.selected = $scope.selected;
+    $cookies.putObject('scales', cookie);
+  };
+
 }]);
