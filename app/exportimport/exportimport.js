f8e40a38a365144d2250c0eef670444032e3cc4d
Matthew Darby <me@mjdarby.net>
2015-07-26 15:52:20 +0100

    Add import functionality, fix default menu behaviour on mobile
    The fact I included jQuery for this just shows how lazy I am.
    Mad shout-outs to StackOverflow for making the <input type="file">
    stuff infinitely less painful.

--- app/exportimport/exportimport.js
+++ app/exportimport/exportimport.js
@@ -1,7 +1,29 @@
 angular.module('myApp.exportimport', [])
 
-.controller('ExportImportController', ['$scope', function($scope) {
+.controller('ExportImportController', ['$scope', '$route', function($scope, $route) {
   var content = JSON.stringify(localStorage);
   var blob = new Blob([ content ], { type : 'text/plain' });
   $scope.url = (window.URL || window.webkitURL).createObjectURL( blob );
+
+  $scope.clickUpload = function() {
+    angular.element('#upload').trigger('click');
+  };
+
+  $scope.fileNameChanged = function(element) {
+    var reader = new FileReader();
+    reader.onload = function() {
+      try {
+        var importedData = reader.result;
+        var localStorageJSON = JSON.parse(importedData);
+        for (key in  localStorageJSON) {
+          localStorage[key] = localStorageJSON[key];
+        }
+        $route.reload();
+      } catch (e) {
+        alert("Didn't manage to import your data!");
+      }
+    };
+    reader.readAsText(element.files[0]);
+    angular.element('#upload').val('');
+  };
 }]);
